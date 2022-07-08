44fc3c3887196ac44efdf6a5411b46602536d6d4
KAHartman-FSO <kahartman@student.fullsail.edu>
2022-07-08 16:37:27 -0400

    Program Stops Running after Window is Closed

--- Graphics Development/Tutorial/Source/WinMain.cpp
+++ Graphics Development/Tutorial/Source/WinMain.cpp
@@ -1,13 +1,24 @@
 #include <windows.h>
 
-#define MAX_NAME_STRING 256
-#define HInstance()GetModuleHandle(NULL)
+		#define MAX_NAME_STRING 256
+		#define HInstance()GetModuleHandle(NULL)
+		
+		WCHAR	WindowClass[MAX_NAME_STRING];
+		WCHAR	WindowTitle[MAX_NAME_STRING];
+		
+		INT		WindowWidth;
+		INT		WindowHeight;
 
-WCHAR	WindowClass[MAX_NAME_STRING];
-WCHAR	WindowTitle[MAX_NAME_STRING];
+		LRESULT CALLBACK WindowProcess(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam)
+		{
+			switch (message) {
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+			}
 
-INT		WindowWidth;
-INT		WindowHeight;
+			return DefWindowProc(hWnd, message, wparam, lparam);
+		}
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 {
@@ -36,7 +47,7 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 
 	wcex.hInstance = HInstance();
 
-	wcex.lpfnWndProc = DefWindowProc;
+	wcex.lpfnWndProc = WindowProcess;
 
 	RegisterClassEx(&wcex);
 
